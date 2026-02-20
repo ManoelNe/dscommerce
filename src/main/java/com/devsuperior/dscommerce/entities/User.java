1334076e81ee35e51453f830a07e369350cbb6ac
ManoelNe <manoel_neto@outlook.com>
2026-02-20 11:34:20 -0300

    feat: cria entidade Category e Product

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscommerce.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,9 +21,14 @@ public class User {
     private Long id;
 
     private String name;
+
+    @Column(unique = true)
     private String email;
+
     private String phone;
+
     private LocalDate birthDate;
+
     private String password;
 
     @OneToMany(mappedBy = "client")
